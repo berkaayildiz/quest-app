253891ea3caa55e90f89b2134fc58242b11ce536
Berkay Yıldız <dev@berkaayildiz.com>
2024-07-06 22:11:02 +0300

    initialize springboot project

---
+++ quest-app-backend/src/main/java/com/berkaayildiz/quest_app_backend/QuestAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.berkaayildiz.quest_app_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestAppBackendApplication.class, args);
+	}
+
+}
